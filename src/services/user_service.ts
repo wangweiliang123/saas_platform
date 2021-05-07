c9b440389c21d1ccc32f3b5848cf358322dbac5d
王维亮 <wangweiliang@chipont.com.cn>
2021-05-07 09:41:27 +0800

    项目

--- src/services/user_service.ts
+++ src/services/user_service.ts
@@ -1,6 +1,6 @@
 const UserServiceSql = require('../db_sqls/mysql_sql/user_sql')
 module.exports = {
-  getUserAll: async (ctx: any, next: any) => {
+  getUserAll: async (ctx: any) => {
     return new Promise((resolve, reject) => {
       ctx.util
         .mysql(UserServiceSql.getUserAll())
